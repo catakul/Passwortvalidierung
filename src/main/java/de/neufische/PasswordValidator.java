ec4806c10a43cf39775567db9bc8851f1589a0ed
Михайло <mihajlo@Air-Mihajlo.fritz.box>
2022-10-28 18:45:09 +0200

    add not RegExp solution

--- src/main/java/de/neufische/PasswordValidator.java
+++ src/main/java/de/neufische/PasswordValidator.java
@@ -1,29 +1,49 @@
 package de.neufische;
 
-/*
-Plan Passwortvalidierung:
-Methode für die Länge des Passwortes
-oder copy paste regex?
- */
-
 public class PasswordValidator {
-    public static void main(String[] args) {
+	static char ch;
 
+	public static void main(String[] args) {
+		System.out.println("Passwortvalidierung");
+		PasswordValidator.containsNumbers("StringWith12345");
+	}
 
-    String password = "lis547uitzutzutut";
-        boolean val = isPasswordLongEnough(password);
-        System.out.println(val);
-    }
-    public static boolean isPasswordLongEnough(String password) {
-        if(password.length()>10){
+	public static boolean isPasswordLongEnough(String s) {
+		if (s.length() <= 10) {
+			return false;
+		}
+		return true;
+	}
 
-            return true;
-        };
-        return false;
-    }
+	public static boolean containsLetters(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			ch = str.charAt(i);
+			if (Character.isUpperCase(ch)) {
+				return true;
+			} else if (Character.isLowerCase(ch)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public static boolean isStringWithLetters(String s) {
+	public static boolean containsNumbers(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			ch = str.charAt(i);
+			for (int j = 0; j < 10; j++) {
+				if (Character.toString(ch).equals("" + j)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-        return true;
-    }
+	public static boolean isSimple(String str) {
+		if (str.contains("password") || (str.contains("123456"))) {
+			return true;
+		}
+		return false;
+	}
+
 }
--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -12,12 +12,62 @@ public class PasswordValidatorTest {
         boolean actual = PasswordValidator.isPasswordLongEnough("Hello03jewrewrewer");
         assertEquals(expected,actual);
     };
-
+    
     @Test
-    public void testIsStringWithLetters()
+    public void testNotGetLengthOfPassword()
     {
-        boolean expected = true;
-        boolean actual = PasswordValidator.isStringWithLetters("Hello");
+        boolean expected = false;
+        boolean actual = PasswordValidator.isPasswordLongEnough("Hello");
         assertEquals(expected,actual);
     };
+
+    @Test
+    public void testContainsStringLetters()
+    {
+        boolean expected = true;
+        boolean actual = PasswordValidator.containsLetters("StringWithoutNumers");
+        assertEquals(expected,actual);
+    };
+    
+    @Test
+    public void testNotContainsStringLetters()
+    {
+        boolean expected = false;
+        boolean actual = PasswordValidator.containsLetters("1234567890");
+        assertEquals(expected,actual);
+    };
+
+    @Test
+    public void testContainsNumbers()
+    {
+        boolean expected = true;
+        boolean actual = PasswordValidator.containsNumbers("StringWith12345");
+        assertEquals(expected,actual);
+    };
+    
+    @Test
+    public void testNotcontainsNumbers()
+    {
+        boolean expected = false;
+        boolean actual = PasswordValidator.containsNumbers("StringWithoutNumers");
+        assertEquals(expected,actual);
+    };
+    
+    @Test
+    public void testIsSimple()
+    {
+        boolean expected = true;
+        boolean actual = PasswordValidator.isSimple("StringWith123456");
+        assertEquals(expected,actual);
+    };
+    
+    @Test
+    public void testIsNotSimple()
+    {
+        boolean expected = false;
+        boolean actual = PasswordValidator.isSimple("StringWithoutNumers");
+        assertEquals(expected,actual);
+    };
+    
+    
 }
