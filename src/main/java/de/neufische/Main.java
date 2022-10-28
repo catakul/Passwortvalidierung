5cbd4d9b7b58709aa470177bc715d555462d6b19
johannachiabotto <johanna.chiabotto@gmail.com>
2022-10-28 11:16:47 +0200

    we don't need this file at all

--- src/main/java/de/neufische/Main.java
+++ src/main/java/de/neufische/Main.java
@@ -1,5 +1,11 @@
 package de.neufische;
 
+/*
+Plan Passwortvalidierung:
+Methode für die Länge des Passwortes
+oder copy paste regex?
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Passwortvalidierung");
